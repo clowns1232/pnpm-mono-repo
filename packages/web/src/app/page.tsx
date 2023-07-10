c3f5575608c26d5b087c0d2e3f17d4ade0f05f2d
황태현 <clowns1232@gmail.com>
2023-07-10 17:21:27 +0900

    chore: README 수정

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,10 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import { Button } from '@pnpm-test/core'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Button } from "@pnpm-test/core";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Button/>
+      <Button
+        style={{
+          backgroundColor: "red",
+          width: "100px",
+          height: "100px",
+          color: "white",
+        }}
+        className="card"
+      >
+        test123
+      </Button>
     </main>
-  )
+  );
 }
