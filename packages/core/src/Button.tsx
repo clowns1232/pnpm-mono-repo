14a9d2bdfb9001b4c7ee22246f1cb9dfcb8293df
황태현 <clowns1232@gmail.com>
2023-07-11 09:42:00 +0900

    modify: next 트렌스파일 라이브러리 및 설정 추가

--- packages/core/src/Button.tsx
+++ packages/core/src/Button.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FC, PropsWithChildren } from "react";
 
 interface Props extends PropsWithChildren {
@@ -7,7 +8,11 @@ interface Props extends PropsWithChildren {
 
 export const Button: FC<Props> = (props) => {
   return (
-    <button style={props.style} className={props.className}>
+    <button
+      style={props.style}
+      className={props.className}
+      onClick={() => alert("test")}
+    >
       {props.children} / 나는 core에서 react로 출력하고 있습니다.
     </button>
   );
