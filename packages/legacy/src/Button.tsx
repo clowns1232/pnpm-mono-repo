8cb7b09fbd14bf196c9c8fd12d24699d1cef0826
황태현 <clowns1232@gmail.com>
2023-07-12 13:48:23 +0900

    build:

---
+++ packages/legacy/src/Button.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export const Button = () => {
+  return (
+    <button
+      onClick={() => {
+        alert("pnpm");
+      }}
+    >
+      레거시코드
+    </button>
+  );
+};
--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Button } from "@pnpm-test/core";
+import { Button as CoreButton } from "@pnpm-test/core";
+import { Button as TestButton } from "@pnpm-test/legacy";
 import { Button as MultButton } from "../../../../mult-repo/src/Button";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>여기는 web이고 next로 구성됩니다.</h1>
-      <Button
+      <CoreButton
         style={{
           backgroundColor: "red",
           width: "100px",
@@ -18,8 +19,9 @@ export default function Home() {
         className="card"
       >
         테스트용 버튼
-      </Button>
+      </CoreButton>
       <MultButton />
+      <TestButton />
     </main>
   );
 }
